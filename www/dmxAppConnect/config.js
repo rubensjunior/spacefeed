cc281242a2302485b831821ce25089f48ced610b
Rubens Bentes <rbntes@gmail.com>
2024-09-26 10:05:18 -0400

    Tabela de colaboradores finalizada

--- www/dmxAppConnect/config.js
+++ www/dmxAppConnect/config.js
@@ -29,5 +29,153 @@ dmx.config({
         "name": "id"
       }
     ]
+  },
+  "colaboradores": {
+    "lista": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id"
+        },
+        {
+          "type": "text",
+          "name": "nome"
+        },
+        {
+          "type": "text",
+          "name": "email"
+        },
+        {
+          "type": "boolean",
+          "name": "isAdmin"
+        },
+        {
+          "type": "boolean",
+          "name": "isColaborador"
+        },
+        {
+          "type": "number",
+          "name": "empresa"
+        },
+        {
+          "type": "boolean",
+          "name": "isAtivo"
+        },
+        {
+          "type": "text",
+          "name": "passSecurity"
+        },
+        {
+          "type": "number",
+          "name": "idempresa"
+        },
+        {
+          "type": "boolean",
+          "name": "academico"
+        },
+        {
+          "type": "boolean",
+          "name": "administrativo"
+        },
+        {
+          "type": "boolean",
+          "name": "comercial"
+        },
+        {
+          "type": "boolean",
+          "name": "diretoria"
+        },
+        {
+          "type": "boolean",
+          "name": "financeiro"
+        },
+        {
+          "type": "boolean",
+          "name": "marketing"
+        },
+        {
+          "type": "boolean",
+          "name": "atendimento"
+        },
+        {
+          "type": "boolean",
+          "name": "ti"
+        }
+      ],
+      "outputType": "array"
+    },
+    "listaColaboradores": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id"
+        },
+        {
+          "type": "text",
+          "name": "nome"
+        },
+        {
+          "type": "text",
+          "name": "email"
+        },
+        {
+          "type": "boolean",
+          "name": "isAdmin"
+        },
+        {
+          "type": "boolean",
+          "name": "isColaborador"
+        },
+        {
+          "type": "number",
+          "name": "empresa"
+        },
+        {
+          "type": "boolean",
+          "name": "isAtivo"
+        },
+        {
+          "type": "text",
+          "name": "passSecurity"
+        },
+        {
+          "type": "number",
+          "name": "idempresa"
+        },
+        {
+          "type": "boolean",
+          "name": "academico"
+        },
+        {
+          "type": "boolean",
+          "name": "administrativo"
+        },
+        {
+          "type": "boolean",
+          "name": "comercial"
+        },
+        {
+          "type": "boolean",
+          "name": "diretoria"
+        },
+        {
+          "type": "boolean",
+          "name": "financeiro"
+        },
+        {
+          "type": "boolean",
+          "name": "marketing"
+        },
+        {
+          "type": "boolean",
+          "name": "atendimento"
+        },
+        {
+          "type": "boolean",
+          "name": "ti"
+        }
+      ],
+      "outputType": "array"
+    }
   }
 });
