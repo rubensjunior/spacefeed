dbafca75aef48e1a9a52b654b8d3b9fee99608bd
Rubens Bentes <rbntes@gmail.com>
2024-10-20 10:41:30 -0400

    Mudança setup.ts

--- electron/src/setup.ts
+++ electron/src/setup.ts
@@ -217,6 +217,7 @@ export class ElectronCapacitorApp {
   }
 }
 
+
 // Set a CSP up for our application based on the custom scheme
 export function setupContentSecurityPolicy(customScheme: string): void {
   session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
