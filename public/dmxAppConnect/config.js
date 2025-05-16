a357fd6a191ba9abc9b5fdea1fb09c0783e8575a
rbentes <rbntes@gmail.com>
2025-05-16 10:24:27 -0400

    Modal editar empresa e passagem de dados para o data detail funcionando.

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,199 @@
+dmx.config({
+  "empresa": {
+    "editEmpresa": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "id_usuario_empresa"
+        },
+        {
+          "type": "datetime",
+          "name": "created_at_usuario_empresa"
+        },
+        {
+          "type": "number",
+          "name": "ref_id_usuario"
+        },
+        {
+          "type": "number",
+          "name": "ref_id_empresa"
+        },
+        {
+          "type": "text",
+          "name": "cargo_usuario_empresa"
+        },
+        {
+          "type": "text",
+          "name": "status_usuario"
+        },
+        {
+          "type": "text",
+          "name": "id_empresa"
+        },
+        {
+          "type": "text",
+          "name": "nome_fantasia"
+        },
+        {
+          "type": "text",
+          "name": "razao_social"
+        },
+        {
+          "type": "text",
+          "name": "nome_polo"
+        },
+        {
+          "type": "text",
+          "name": "cnpj"
+        },
+        {
+          "type": "text",
+          "name": "endereco"
+        },
+        {
+          "type": "text",
+          "name": "cep"
+        },
+        {
+          "type": "text",
+          "name": "whatsapp"
+        },
+        {
+          "type": "text",
+          "name": "status_empresa"
+        }
+      ],
+      "outputType": "array"
+    },
+    "data_empresa": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "id_usuario_empresa"
+        },
+        {
+          "type": "datetime",
+          "name": "created_at_usuario_empresa"
+        },
+        {
+          "type": "number",
+          "name": "ref_id_usuario"
+        },
+        {
+          "type": "number",
+          "name": "ref_id_empresa"
+        },
+        {
+          "type": "text",
+          "name": "cargo_usuario_empresa"
+        },
+        {
+          "type": "text",
+          "name": "status_usuario"
+        },
+        {
+          "type": "text",
+          "name": "id_empresa"
+        },
+        {
+          "type": "text",
+          "name": "nome_fantasia"
+        },
+        {
+          "type": "text",
+          "name": "razao_social"
+        },
+        {
+          "type": "text",
+          "name": "nome_polo"
+        },
+        {
+          "type": "text",
+          "name": "cnpj"
+        },
+        {
+          "type": "text",
+          "name": "endereco"
+        },
+        {
+          "type": "text",
+          "name": "cep"
+        },
+        {
+          "type": "text",
+          "name": "whatsapp"
+        },
+        {
+          "type": "text",
+          "name": "status_empresa"
+        }
+      ],
+      "outputType": "array"
+    },
+    "data_detail1": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "id_usuario_empresa"
+        },
+        {
+          "type": "datetime",
+          "name": "created_at_usuario_empresa"
+        },
+        {
+          "type": "number",
+          "name": "ref_id_usuario"
+        },
+        {
+          "type": "number",
+          "name": "ref_id_empresa"
+        },
+        {
+          "type": "text",
+          "name": "cargo_usuario_empresa"
+        },
+        {
+          "type": "text",
+          "name": "status_usuario"
+        },
+        {
+          "type": "text",
+          "name": "id_empresa"
+        },
+        {
+          "type": "text",
+          "name": "nome_fantasia"
+        },
+        {
+          "type": "text",
+          "name": "razao_social"
+        },
+        {
+          "type": "text",
+          "name": "nome_polo"
+        },
+        {
+          "type": "text",
+          "name": "cnpj"
+        },
+        {
+          "type": "text",
+          "name": "endereco"
+        },
+        {
+          "type": "text",
+          "name": "cep"
+        },
+        {
+          "type": "text",
+          "name": "whatsapp"
+        },
+        {
+          "type": "text",
+          "name": "status_empresa"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
