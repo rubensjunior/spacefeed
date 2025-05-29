59f8303c15a84bdde706b74e02b17183bd7c0040
rbentes <rbntes@gmail.com>
2025-05-29 13:33:28 -0400

    Feito Pagina de escolha de empresa com condicionais.

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -160,5 +160,96 @@ dmx.config({
       "meta": [],
       "outputType": "number"
     }
+  },
+  "escolhaempresa": {
+    "repeatEmpresas": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "id_usuario_empresa"
+        },
+        {
+          "type": "datetime",
+          "name": "created_at_usuario_empresa"
+        },
+        {
+          "type": "number",
+          "name": "ref_id_usuario"
+        },
+        {
+          "type": "number",
+          "name": "ref_id_empresa"
+        },
+        {
+          "type": "text",
+          "name": "cargo_usuario_empresa"
+        },
+        {
+          "type": "text",
+          "name": "status_usuario"
+        },
+        {
+          "type": "text",
+          "name": "id_empresa"
+        },
+        {
+          "type": "datetime",
+          "name": "created_at_empresa"
+        },
+        {
+          "type": "text",
+          "name": "razao_social"
+        },
+        {
+          "type": "text",
+          "name": "nome_fantasia"
+        },
+        {
+          "type": "text",
+          "name": "nome_polo"
+        },
+        {
+          "type": "text",
+          "name": "cnpj"
+        },
+        {
+          "type": "text",
+          "name": "cep"
+        },
+        {
+          "type": "text",
+          "name": "whatsapp"
+        },
+        {
+          "type": "text",
+          "name": "status_empresa"
+        },
+        {
+          "type": "text",
+          "name": "logradouro"
+        },
+        {
+          "type": "text",
+          "name": "numero"
+        },
+        {
+          "type": "text",
+          "name": "complemento"
+        },
+        {
+          "type": "text",
+          "name": "bairro"
+        },
+        {
+          "type": "text",
+          "name": "cidade"
+        },
+        {
+          "type": "text",
+          "name": "uf"
+        }
+      ],
+      "outputType": "array"
+    }
   }
 });
